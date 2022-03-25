76ae0b19f3da4f6ac90e4b2d69191b07754af170
Léo Levavasseur <22001156@etu.unicaen.fr>
2022-03-25 16:28:02 +0100

    partie B non finie

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,3 +1,16 @@
-import Route from '@ember/routing/route';
+import Abstractroute from './AbstractRoute';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class OrderRoute extends Route {}
+export default class OrderRoute extends Abstractroute {
+  @service router;
+  @service store;
+
+  model(params) {
+    return this.store.findRecord('order',params.order_id,{include : 'user,orderdetails.product'})
+  }
+
+  @action return(){
+    this.transitionTo('board');
+  }
+}
